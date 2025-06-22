fdea77f5943f3317c441c14583e3b6319c67a50a
Glenn <edstrom.glenn@gmail.com>
2025-06-22 12:41:12 -0500

    removed spaces from competitive programming problems

--- lua/custom/plugins/competitest.lua
+++ lua/custom/plugins/competitest.lua
@@ -5,40 +5,44 @@ return {
     require('competitest').setup {
       -- Basic configuration
       local_config_file_name = '.competitest.lua',
-
       -- Language-specific settings
       compile_command = {
         cpp = { exec = 'g++', args = { '-Wall', '-O2', '-std=c++17', '$(FNAME)', '-o', '$(FNOEXT)' } },
         c = { exec = 'gcc', args = { '-Wall', '-O2', '$(FNAME)', '-o', '$(FNOEXT)' } },
       },
-
       run_command = {
         cpp = { exec = './$(FNOEXT)' },
         c = { exec = './$(FNOEXT)' },
       },
-
       -- Template settings
       template_file = {
         cpp = '~/.config/nvim/templates/cp.cpp',
       },
-
       -- Test case settings
       testcases_directory = './tests',
       testcases_use_single_file = true,
-
       -- UI settings
       split_direction = 'horizontal',
       popup_width = 95,
       popup_height = 80,
-
       -- Automatically save before running tests
       save_current_file = true,
-
       -- Show test case details
       show_nu = true,
       show_rnu = true,
-    }
 
+      -- Fix for spaces in filenames: Use JAVA_TASK_CLASS modifier
+      -- This removes all non-alphabetic and non-numeric characters, including spaces
+      received_problems_path = '$(CWD)/$(JAVA_TASK_CLASS).$(FEXT)',
+      received_contests_problems_path = '$(JAVA_TASK_CLASS).$(FEXT)',
+
+      -- Alternative: Use a custom function if you want more control
+      -- received_problems_path = function(task, file_extension)
+      --   local problem_name = task.name:gsub('%s+', '_') -- Replace spaces with underscores
+      --   problem_name = problem_name:gsub('[^%w_-]', '') -- Remove other special characters
+      --   return string.format('%s/%s.%s', vim.fn.getcwd(), problem_name, file_extension)
+      -- end,
+    }
     -- Keymaps
     vim.keymap.set('n', '<leader>cr', '<cmd>CompetiTest run<CR>', { desc = '[C]ompetitive [R]un tests' })
     vim.keymap.set('n', '<leader>ca', '<cmd>CompetiTest add_testcase<CR>', { desc = '[C]ompetitive [A]dd test case' })
