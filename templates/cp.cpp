7c93d6673500060db1729881c3a9872dc3b75047
glenn <edstrom.glenn@gmail.com>
2025-08-26 18:49:36 -0500

    updated templates and snippets

--- templates/cp.cpp
+++ templates/cp.cpp
@@ -1,10 +1,19 @@
+#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 using namespace std;
-#define YES cout << "YES" << endl;
-#define NO cout << "NO" << endl;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+// EPS for doubles; do 1e-6 for floats
+#define EPS 1e-9
+#define INF 1e9
 
 void solve() {
     // TODO:
 }
 
-int main(){int t;cin>>t;while(t--){solve();ios_base::sync_with_stdio(false);cin.tie(NULL);}}//{solve();ios_base::sync_with_stdio(false);cin.tie(NULL);}
+int main(){int t;cin>>t;while(t--){solve();}ios_base::sync_with_stdio(false);cin.tie(NULL);}//{solve();ios_base::sync_with_stdio(false);cin.tie(NULL);}
