7f6bb3fd0911e9134041b1a8b2f512ff5fc68a70
glenn <edstrom.glenn@gmail.com>
2025-09-08 22:43:15 -0500

    updated python templaet

--- templates/cp.py
+++ templates/cp.py
@@ -1,8 +1,15 @@
 import sys
 def input():
     return sys.stdin.readline().strip()
+import math
+from math import *
 import collections
-import queue
+from collections import ChainMap, Counter, OrderedDict, UserDict, UserList, UserString, defaultdict
+import itertools
+from itertools import combinations, combinations_with_replacement, count, groupby, permutations
+import heapq
+from heapq import nlargest, nsmallest
+from queue import Queue, PriorityQueue
 
 def solve():
     pass
